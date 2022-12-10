e73bb9ed51d513f6da1ea2b4929d115fab17ca61
Tom Tom X <tom.tom@x.com>
2022-12-10 13:21:53 +0100

    vjezba film

---
+++ vjezba_film.py
@@ -0,0 +1,17 @@
+
+from operator import truediv
+
+
+movie = "Out Of Africa"
+release = "1984"
+director = "Merly Streep"
+lengt = 123
+
+
+print("Movie ",movie," is Oscar winner of ",release," directed by ",director,".", sep="")
+
+print(f'Movie {movie} is the Oscar winner of {release}, directed by {director}.')
+
+class sas :
+    (
+    print)
