610bc1aa54cdd8f6b77d30092db126301ea235a3
Tom Tom X <tom.tom@x.com>
2022-12-10 11:01:47 +0100

    Tipovi podataka

---
+++ varijable_tipovi_podataka.py
@@ -0,0 +1,10 @@
+
+from cgi import print_exception
+
+
+price_per_kw = 1 
+mw_consumption = 1.3
+total_consumption = ((2 * 30) * 1.3) 
+total_consumption_price = total_consumption * 1
+print("Total electricty consuption:", total_consumption)
+print("Total electricty consuption price:", total_consumption_price)
